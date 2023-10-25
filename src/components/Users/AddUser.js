97de055733acec3b611ada6d703a739bfde1116b
alisalali <alis.alali@hotmail.com>
2023-10-26 02:50:28 +0300

    UI component design

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,19 +1,40 @@
 import React from "react";
-import style from "./AddUser.module.css";
+import classes from "./AddUser.module.css";
+import Card from "../UI/Card";
+import Button from "../UI/Button";
+
+
 function AddUser() {
   const addUserHandler = (event) => {
     event.preventDefault();
   };
+  const onInputHandler = (id, value) => {
+    console.log(id, value);
+  };
   return (
-    <div className={style.card}>
-      <form onSubmit={addUserHandler} class={style["form-group"]}>
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler} class={classes["form-group"]}>
         <label htmlFor="username">username</label>
-        <input type="text" name="" id="username"></input>
+        <input
+          type="text"
+          name=""
+          id="username"
+          onChange={(event) => {
+            onInputHandler(event.target.id, event.target.value);
+          }}
+        ></input>
         <label htmlFor="age">Age</label>
-        <input type="number" name="" id="age"></input>
-        <button type="button">Add User</button>
+        <input
+          type="number"
+          name=""
+          id="age"
+          onChange={(event) => {
+            onInputHandler(event.target.id, event.target.value);
+          }}
+        ></input>
+        <Button type="button">Add User</Button>
       </form>
-    </div>
+    </Card>
   );
 }
 
